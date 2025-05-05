47dd95a70387e9f6c55bbbce2de10a203062c10c
YordanVuchev <yordanyvuchev@gmail.com>
2025-05-05 12:25:50 +0300

    changed sol version

--- test/unit/raptor-unit-tests/RaptorNFTTest.t.sol
+++ test/unit/raptor-unit-tests/RaptorNFTTest.t.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.28;
 
 import {RaptorNFTBaseTest} from "./RaptorNFTBaseTest.t.sol";
 import {RaptorNFT} from "../../../src/RaptorNFT.sol";
