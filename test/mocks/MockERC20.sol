7041f41b76cf85ec618044e8eb25d950149f75cb
YordanVuchev <yordanyvuchev@gmail.com>
2025-05-05 11:42:36 +0300

    Added BaseTest for RaptorNFT

--- test/mocks/MockERC20.sol
+++ test/mocks/MockERC20.sol
@@ -4,17 +4,17 @@ pragma solidity ^0.8.20;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
-  constructor(uint8 decimals_) ERC20("Mock", "MM") {
-      _decimals = decimals_;
-  }
+    constructor(uint8 decimals_) ERC20("Mock", "MM") {
+        _decimals = decimals_;
+    }
 
-  uint8 private _decimals;
+    uint8 private _decimals;
 
-  function decimals() public view override returns (uint8) {
-      return _decimals;
-  }
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
 
-  function mint(address to, uint256 amount) external {
-      _mint(to, amount);
-  }
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
 }
---
+++ test/unit/raptor-unit-tests/RaptorNFTBaseTest.t.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {RaptorNFT} from "../../../src/RaptorNFT.sol";
+import {MockV3Aggregator} from "../../mocks/MockV3Aggregator.sol";
+import {MockERC20} from "../../mocks/MockERC20.sol";
+
+contract RaptorNFTBaseTest is Test {
+    RaptorNFT nft;
+    MockV3Aggregator mockPriceFeed;
+    MockERC20 usdc;
+
+    string constant NFT_NAME = "Raptor";
+    string constant NFT_SYMBOL = "RR";
+    string constant NFT_URI = "ipfs://bafkreihouvejsacfci5g67bbrsyxstp2g3vt4w6ctqd3fd6p5kssykdjfa";
+    address OWNER = makeAddr("owner");
+    address BOB = makeAddr("bob");
+
+    int256 INITIAL_ETH_USD_PRICE = 2000e18;
+    uint256 public constant INITIAL_NFT_PRICE = 50e18;
+    uint256 public constant INITIAL_USER_BALANCE = 10 ether;
+    uint256 public constant INITIAL_USER_STABLE_BALANCE = 1000e6;
+
+    function setUp() public virtual {
+        mockPriceFeed = new MockV3Aggregator(18, INITIAL_ETH_USD_PRICE);
+        nft = new RaptorNFT(INITIAL_NFT_PRICE, address(mockPriceFeed), OWNER);
+        usdc = new MockERC20(6);
+
+        vm.deal(BOB, INITIAL_USER_BALANCE);
+        usdc.mint(BOB, INITIAL_USER_STABLE_BALANCE);
+    }
+}
